28e3901a42d4488d8c81eb60188acc5f2c6f8841
okorokovnikita <57351674+okorokovnikita@users.noreply.github.com>
2019-11-25 10:35:27 +0500

    Add files via upload

---
+++ two_sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+        for i, v in enumerate(nums):
+            remaining = target - v
+            if remaining in seen:
+                return [seen[remaining], i]
+            else:
+                seen[v] = i
+        return []
+
