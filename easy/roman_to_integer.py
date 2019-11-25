4f71949d9451467c16022bf933d73b614de5666d
okorokovnikita <57351674+okorokovnikita@users.noreply.github.com>
2019-11-25 15:42:13 +0500

    Add files via upload

---
+++ easy/roman_to_integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def romanToInt(self, s):
+        num = 0
+        dic = {'I': 1,
+             'V': 5,
+             'X': 10,
+             'L': 50,
+             'C': 100,
+             'D': 500,
+             'M': 1000}
+        i = 0
+        while i < len(s)-1:
+            if dic[s[i]] >= dic[s[i+1]]:
+                num = num + dic[s[i]]
+                i = i + 1
+            else:
+                num = num + dic[s[i+1]] - dic[s[i]]
+                i = i + 2
+        if i==len(s) - 1:
+                num = num + dic[s[len(s)-1]]
+        return num
+    
