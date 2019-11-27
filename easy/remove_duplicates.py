ec0dea47eb77c059119a64e0d0c92a1959036cf4
okorokovnikita <57351674+okorokovnikita@users.noreply.github.com>
2019-11-27 10:56:21 +0500

    Delete remove_duplicates.py

--- easy/remove_duplicates.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums):
-        i = 0
-        while i < len(nums) - 1:
-            if nums[i] == nums[i+1]:
-                del nums[i]
-            else:
-                i=i+1
-        return len(nums)
