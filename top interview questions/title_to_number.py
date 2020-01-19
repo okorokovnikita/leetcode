e3a8e8c3d6a3d66874f992f3ebfc85a96847d728
okorokovnikita <57351674+okorokovnikita@users.noreply.github.com>
2020-01-19 15:10:01 +0500

    Create title_to_number.py

---
+++ top interview questions/title_to_number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        result=0
+        for i in range(len(s)):
+            result= result*26 + ord(s[i]) - ord('A') +1
+        return result
