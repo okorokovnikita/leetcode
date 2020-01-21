3c8b3ae2bb5c8b2b4d62fc4ce2f830d4a9dc19e3
okorokovnikita <57351674+okorokovnikita@users.noreply.github.com>
2020-01-21 10:12:46 +0500

    Update unique_numbers_sum_zero.py

--- arrays/unique_numbers_sum_zero.py
+++ arrays/unique_numbers_sum_zero.py
@@ -5,8 +5,7 @@ class Solution:
         result=[]
         for i in range(1,int(n/2)+1):
             result.append(i)
-        for i in range(-1,-int(n/2)-1,-1):
-            result.append(i)
+            result.append(-i)
         if n%2!=0:
             result.append(0)
         return result
